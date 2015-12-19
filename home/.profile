fda93be4b2ec2b310e581bd76d4a798efa00df21
jonnew <jpnewman@mit.edu>
2015-12-19 13:49:05 -0500

    Updated/added dot files for xmonad and more generic key repeat settings across distros.

--- home/.profile
+++ home/.profile
@@ -8,18 +8,11 @@
 # for ssh logins, install and configure the libpam-umask package.
 #umask 022
 
-# if running bash
-if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
-    if [ -f "$HOME/.bashrc" ]; then
-	. "$HOME/.bashrc"
-    fi
-fi
+# Set key repeat rate 
+xset r rate 150 80
 
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
+# Turn off the touchpad
+xinput set-prop 11 "Device Enabled" 0
 
-# For solarized color scheme
-#TERM='xterm-256color'
+# Adjust pointer sensitivity
+xinput --set-prop 12 263 0.5
---
+++ home/.xmonad/xmobar.hs
@@ -0,0 +1,30 @@
+-- xmobar config used by Vic Fryzel
+-- Author: Vic Fryzel
+-- http://github.com/vicfryzel/xmonad-config
+
+-- This is setup for dual 1920x1080 monitors, with the right monitor as primary
+Config {
+    font = "xft:Fixed-14",
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    position = Top,
+    --position = Static { xpos = 0, ypos = 0, width = 2560, height = 20 },
+    lowerOnStart = True,
+    commands = [
+        Run BatteryP ["BAT0"] [ "-t", "<acstatus>"
+                              , "-L", "10", "-H", "80"
+                              , "-l", "red", "-h", "green"
+                              , "--", "-O", "Charging", "-o", "Battery: <left>%"
+                              ] 10, 
+        -- Run Battery ["-t","<left>% / <timeleft>","-L","50","-H","75","-h","green","-n","yell","-l","red"] 10 
+        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "wlan0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = " %StdinReader% }{ %battery%  |  %multicpu%   %memory%   %swap%  |  %wlan0%  |  <fc=#FFFFCC>%date%</fc> "
+}
